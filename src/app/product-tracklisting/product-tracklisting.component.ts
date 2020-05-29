1e7cdd34cc2ae74b0833921be95426632829d547
Jacob Pine <jpinedev@gmail.com>
2020-05-29 13:34:51 -0400

    added tracklist populated by albumInfo.album.tracks

--- src/app/product-tracklisting/product-tracklisting.component.ts
+++ src/app/product-tracklisting/product-tracklisting.component.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-
 import { ProductService } from '../product.service';
+import { Album } from '../album';
 import { AlbumInfo } from '../album-info';
 
 @Component({
-  selector: 'app-product-description',
-  templateUrl: './product-description.component.html',
-  styleUrls: ['./product-description.component.css']
+  selector: 'app-product-tracklisting',
+  templateUrl: './product-tracklisting.component.html',
+  styleUrls: ['./product-tracklisting.component.css']
 })
-export class ProductDescriptionComponent implements OnInit {
+export class ProductTracklistingComponent implements OnInit {
 
   albumInfo: AlbumInfo;
 
--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
-import { Album } from './album';
 import { Observable } from 'rxjs/Observable';
+import { AlbumInfo } from './album-info';
 
 @Injectable()
 export class ProductService {
@@ -11,8 +11,8 @@ export class ProductService {
 
   constructor(private _http: Http) { }
 
-  getAlbum(id: number): Observable<Album> {
-    return this._http.get(this._albumUrl).map(response => <Album>response.json());
+  getAlbum(id: number): Observable<AlbumInfo> {
+    return this._http.get(this._albumUrl).map(response => <AlbumInfo>response.json());
   }
 
 }
